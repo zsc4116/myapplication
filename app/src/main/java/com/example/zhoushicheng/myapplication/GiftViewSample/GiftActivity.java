d2d411aad6762d37be88655fdfdbebb1f158a735
zsc <zsc4116@163.com>
2017-08-29 22:17:58 +0800

    添加GiftViewSample

---
+++ app/src/main/java/com/example/zhoushicheng/myapplication/GiftViewSample/GiftActivity.java
@@ -0,0 +1,15 @@
+package com.example.zhoushicheng.myapplication.GiftViewSample;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.example.zhoushicheng.myapplication.R;
+
+public class GiftActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_gift);
+    }
+}
