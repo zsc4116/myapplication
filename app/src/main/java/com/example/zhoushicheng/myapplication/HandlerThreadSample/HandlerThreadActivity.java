1822d36b2a59e70b5ae34c3b624f2ec7e29035d8
zsc4116 <zsc4116@163.com>
2017-11-03 10:44:23 +0800

    HandlerThreadSample

---
+++ app/src/main/java/com/example/zhoushicheng/myapplication/HandlerThreadSample/HandlerThreadActivity.java
@@ -0,0 +1,89 @@
+package com.example.zhoushicheng.myapplication.HandlerThreadSample;
+
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.HandlerThread;
+import android.os.Message;
+import android.support.v7.app.AppCompatActivity;
+import android.text.Html;
+import android.widget.TextView;
+
+import com.example.zhoushicheng.myapplication.R;
+
+public class HandlerThreadActivity extends AppCompatActivity {
+    private TextView mTvContent;
+
+    private HandlerThread mCheckMsgThread;
+    private Handler mCheckMsgHandler;
+    private boolean isUpdateInfo;
+
+    private static final int MSG_UPDATE_INFO = 0x110;
+
+    private Handler mHandler = new Handler();
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_handler_thread);
+
+        mTvContent = (TextView) findViewById(R.id.tv_content);
+
+        initBackThread();
+
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        isUpdateInfo = true;
+        mCheckMsgHandler.sendEmptyMessage(MSG_UPDATE_INFO);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        isUpdateInfo = false;
+        mCheckMsgHandler.removeMessages(MSG_UPDATE_INFO);
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        mCheckMsgThread.quit();
+    }
+
+    private void initBackThread() {
+        mCheckMsgThread = new HandlerThread("check-message-coming");
+        mCheckMsgThread.start();
+        mCheckMsgHandler = new Handler(mCheckMsgThread.getLooper()) {
+
+            @Override
+            public void handleMessage(Message msg) {
+                super.handleMessage(msg);
+                checkForUpdate();
+                if (isUpdateInfo) {
+                    mCheckMsgHandler.sendEmptyMessageDelayed(MSG_UPDATE_INFO, 1000);
+                }
+
+            }
+        };
+    }
+
+    private void checkForUpdate() {
+        mHandler.post(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    Thread.sleep(1000);
+                    String result = "实时更新中，当前大盘指数:<font color = 'red'>%d</font>";
+                    result = String.format(result, (int) (Math.random() * 3000 + 1000));
+                    mTvContent.setText(Html.fromHtml(result));
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+    }
+
+}
